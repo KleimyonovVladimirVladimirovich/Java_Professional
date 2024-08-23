d3b51507cbbf52f1ec460c29f7f7f496131dfdfb
Vladimir <kitkatorgnew@gmail.com>
2024-08-23 20:53:31 +0300

    Initial

--- src/main/java/json/Solution.java
+++ src/main/java/json/Solution.java
@@ -1,28 +1,27 @@
 package json;
 
 
-import java.lang.reflect.Type;
 import java.util.*;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.annotations.SerializedName;
+import lombok.Builder;
+import lombok.Data;
 
-
+@Builder
+@Data
 public class Solution {
-    private static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            //.setPrettyPrinting() переводит каждый раздел на новую строку, не баг а фича
+            private static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
-    public static void main(String[] args) {
-        Person person = new Person("Vladimir", 20, List.of("Moscow", "Saint-Petersburg", "Postov-on-Don"));
-        String json = gson.toJson(person);
-        System.out.println(json);
+            public static void main(String[] args) {
+                Person person = new Person("Vladimir", 20, List.of("Moscow", "Saint-Petersburg", "Postov-on-Don"));
+                String json = gson.toJson(person);
+                System.out.println(json);
 
         Person person1 = new Person("Di", 14, List.of("China"));
         String json1 = gson.toJson(person1);
         System.out.println(json1);
-
-
-
-
     }
 }
 
@@ -32,9 +31,9 @@ class Person {
     @SerializedName("History") //Заменяет название geoHistory
     private List<String> geoHistory = new ArrayList<>();
 
-    public Person(String name, Integer age, List<String> geohist) {
+    public Person(String name, Integer age, List<String> geoHistory) {
         this.name = name;
         this.age = age;
-        this.geoHistory = geohist;
+        this.geoHistory = geoHistory;
     }
 }
---
+++ src/main/java/kleimyonov/Main.java
@@ -0,0 +1,11 @@
+package kleimyonov;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello mazafaka");
+
+
+    }
+}
